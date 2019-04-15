15a8d7f88a9113b0e03b04f1317b81094851326b
Wing-Snyper <34709335+Wing-Snyper@users.noreply.github.com>
2019-04-15 23:14:09 +0400

    Add files via upload

---
+++ MongoDBExample/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver.GridFS;
+using MongoDBExample.Models;
+
+namespace MongoDBExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public readonly ApplicationDbContext ContextNew = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+            ObjectId uploadSuccess;
+            foreach (var formFile in files)
+            {
+                if (formFile.Length <= 0)
+                {
+                    continue;
+                }
+                using (var stream = formFile.OpenReadStream())
+                {
+                    uploadSuccess = await MongoUpload(formFile.FileName, stream);
+                    ViewBag.Message = uploadSuccess;
+                }
+            }
+
+            return View("UploadSuccess");
+        }
+        private async Task<ObjectId> MongoUpload(string filename, Stream stream)
+        {
+
+            var options = new GridFSUploadOptions
+            {
+                Metadata = new BsonDocument
+                {
+                    {"ContentType","image/jpeg"}
+                }
+            };
+            var id = await ContextNew.ImagesBucket.UploadFromStreamAsync(filename, stream, options);
+            return id;
+            //if (id == null)
+            //    return false;
+            //else
+            //    return true;
+        }
+
+
+        public ActionResult MongoDownload(string id)
+        {
+            try
+            {
+                var stream = ContextNew.ImagesBucket.OpenDownloadStream(new ObjectId(id));
+                var contentType = stream.FileInfo.Metadata["ContentType"].AsString;
+                return File(stream, contentType);
+            }
+            catch (GridFSFileNotFoundException)
+            {
+                return HttpNotFound();
+            }
+        }
+
+        private ActionResult HttpNotFound()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MongoDBExample/Models/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDBExample.Models
+{
+    public class ApplicationDbContext
+    {
+        public IMongoDatabase Database;
+
+        public GridFSBucket ImagesBucket { get; set; }
+
+        public ApplicationDbContext()
+        {
+            var connectionString = "mongodb://localhost:27017";
+            var settings = MongoClientSettings.FromUrl(new MongoUrl(connectionString));
+            var client = new MongoClient(settings);
+            Database = client.GetDatabase("Images");
+            ImagesBucket = new GridFSBucket(Database);
+        }
+    }
+
+}
